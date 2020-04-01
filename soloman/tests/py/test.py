e636f68f9b63c8bef96f5e270e5133b255adfb1f
amoh-godwin <amohgyebigodwin@gmail.com>
2020-04-01 21:53:35 +0000

    change loc

---
+++ soloman/tests/py/test.py
@@ -0,0 +1,5 @@
+from ......soloman import Audio
+
+aud = Audio()
+aud.prepare('H:/GitHub/soloman/soloman/audio/data/music/f.mp3')
+aud.delay_play(0.00000)
--- test.py
+++
@@ -1,5 +0,0 @@
-from swift import Audio
-
-aud = Audio()
-aud.prepare('H:/GitHub/SwiftMultimedia/audio/data/music/f.mp3')
-aud.delay_play(0.00000)
