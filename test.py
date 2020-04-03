efff768221316716018f50a7b9bbebe3d6dd8900
amoh-godwin <amohgyebigodwin@gmail.com>
2020-04-03 16:18:39 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
+from time import sleep
 from soloman import Audio
 
 aud = Audio()
 aud.prepare('H:/GitHub/soloman/soloman/audio/data/music/f.mp3')
-print('her')
 aud.delay_play(0.00000)
+sleep(.1)
 
-if aud.playing:
-    print('hee')
+#aud.stop()
+print(aud)
+
