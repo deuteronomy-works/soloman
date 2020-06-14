45f96cbe57ff8a7519b6c513f2a399ee68b7a16f
amoh-godwin <amohgyebigodwin@gmail.com>
2020-06-14 11:59:05 +0000

    version 2.0.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as rm:
 
 setup(
     name='soloman',
-    version='2.0',
+    version='2.0.1',
     description='For the love of python and qml',
     long_description=long_desc,
     long_description_content_type='text/markdown',
