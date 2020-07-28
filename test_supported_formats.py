9a94dd007ec8239f8c11df09dbfaca7fd5680650
amoh-godwin <amohgyebigodwin@gmail.com>
2020-07-28 20:12:16 +0000

    Add test for supported formats, more of a fix for #28

---
+++ test_supported_formats.py
@@ -0,0 +1,27 @@
+"""
+Test for supported formats
+"""
+import os
+import pytest
+from soloman import QVideo
+
+EXAMPLE_PATH = os.path.join(os.path.dirname(__file__), 'tests', 'videos')
+
+@pytest.mark.parametrize('fileName', [
+    (str(os.path.join(EXAMPLE_PATH, 'countdown.mp4')))
+    ])
+def test_supported_formats(file_name):
+    """
+    The main function to test the supported formats
+    """
+
+    q_vid = QVideo()
+    q_vid.convert_to_stills(file_name)
+
+    # check number of files
+    count = len(os.listdir(q_vid.folder))
+
+    # Delete the contents before assert
+    os.system('RD "' + q_vid.folder + '" /S /Q')
+
+    assert count != 0
