44a342a50647707860ffb0abd9954cd2f101403d
amoh-godwin <amohgyebigodwin@gmail.com>
2020-07-31 10:59:12 +0000

    add tests for fps function

---
+++ test_fps_clocks.py
@@ -0,0 +1,21 @@
+from timeit import timeit
+from time import sleep
+
+import pytest
+
+import soloman
+
+@pytest.mark.parametrize('func', [
+    (soloman.fps_24),
+    (soloman.fps_30),
+    (soloman.fps_60)])
+def test_fps_availability(func):
+    assert func() == True
+
+@pytest.mark.parametrize('func,duration', [
+    (soloman.fps_24, 1/40),
+    (soloman.fps_30, 1/60),
+    (soloman.fps_60, 1/120)])
+def test_fps_sleep_durations(func, duration):
+    dura = timeit(func, number=1)
+    assert round(dura,4) <= duration
