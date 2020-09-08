bf46413c228c16fb769485529c6fd646216524d8
amoh-godwin <amohgyebigodwin@gmail.com>
2020-09-08 01:23:01 +0000

    build 2.4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as rm:
 
 setup(
     name='soloman',
-    version='2.4-dev',
+    version='2.4-beta',
     description='For the love of python and qml',
     long_description=long_desc,
     long_description_content_type='text/markdown',
@@ -23,7 +23,7 @@ setup(
     classifiers = [
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Development Status :: 5 - Production/Stable",
+        "Development Status :: 4 - Beta",
         "Environment :: Other Environment",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
