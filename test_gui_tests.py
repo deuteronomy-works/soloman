b98e59c76c67d86d0af4bea04e32d4d97d405e41
amoh-godwin <amohgyebigodwin@gmail.com>
2021-04-19 17:55:59 +0000

    use width and height of app

--- test_gui_tests.py
+++ test_gui_tests.py
@@ -4,9 +4,13 @@ Created on 18th April, 2021
 import os
 import subprocess
 import threading
+from time import sleep
 
 import pyautogui
 
+
+WIDTH = 800
+HEIGHT = 600
 
 def show_local():
     s_thread = threading.Thread(target=_show_local)
@@ -19,4 +23,19 @@ def _show_local():
     print(os.system('python local_t_est.py'))
 
 
+def auto_gui():
+    auto_thread = threading.Thread(target=_auto_gui)
+    auto_thread.daemon = True
+    auto_thread.start()
+
+
+def _auto_gui():
+    print('dsdf')
+    s_width, s_height = pyautogui.size()
+    print(s_width, s_height, pyautogui)
+
+
 show_local()
+auto_gui()
+
+sleep(10)
