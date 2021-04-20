e2608989234c5609fb55f6bafa8043129d2d8dff
amoh-godwin <amohgyebigodwin@gmail.com>
2021-04-20 10:42:00 +0000

    add test video contents

--- test_a_tests.py
+++ test_a_tests.py
@@ -9,6 +9,10 @@ def test_tests_folder():
     exist = os.path.exists(PREF_PATH)
     assert exists == True
 
+def test_tests_videos_folder():
+    ex = os.path.exists(PREF_PATH + '/videos')
+    assert exists == True
+
 def test_tests_contents():
     conts = os.listdir(PREF_PATH + '/videos')
     assert len(conts) > 0
