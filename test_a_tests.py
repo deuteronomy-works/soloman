541522c09802e39e02758c994cfd9f62ee44713e
amoh-godwin <amohgyebigodwin@gmail.com>
2021-04-20 10:43:43 +0000

    Update test_a_tests.py

--- test_a_tests.py
+++ test_a_tests.py
@@ -7,11 +7,11 @@ PREF_PATH = './tests'
 
 def test_tests_folder():
     exist = os.path.exists(PREF_PATH)
-    assert exists == True
+    assert exist == True
 
 def test_tests_videos_folder():
-    ex = os.path.exists(PREF_PATH + '/videos')
-    assert exists == True
+    exist = os.path.exists(PREF_PATH + '/videos')
+    assert exist == True
 
 def test_tests_contents():
     conts = os.listdir(PREF_PATH + '/videos')
