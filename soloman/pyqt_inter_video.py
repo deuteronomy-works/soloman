96b0a9e4c3a027f7fa26b2f833a1227b62dce0eb
amoh-godwin <amohgyebigodwin@gmail.com>
2021-05-13 12:52:09 +0000

    has increase speed at least

--- soloman/pyqt_inter_video.py
+++ soloman/pyqt_inter_video.py
@@ -150,7 +150,7 @@ class QVideo(QQuickItem):
         out = self.folder + "vid_%01d.jpg"
         start_frame = str(start_frame)
         cmd = f"-i {self._curr_file} -ss {time}"
-        cmd += f" -vf fps={str(self.fps)} -start_number {start_frame} {out}"
+        cmd += f" -r {str(self.fps)} -start_number {start_frame} {out}"
         self._ffmpeg_inst.quit('options')
         self._ffmpeg_inst.options(cmd)
         print('err: ', self._ffmpeg_inst.error)
